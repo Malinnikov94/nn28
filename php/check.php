242baff180971440f72574d8c8133ecaa4be9f12
Malinnikov94 <malinnikov94@gmail.com>
2020-05-19 00:55:07 +0200

    W

--- php/check.php
+++ php/check.php
@@ -1,5 +1,10 @@
 <?php
-  $_GET
-  $login = $_POST['login'];
-  echo $login;
+  $login = filter_var(trim($_POST['login']), FILTER_SANITIZE_STRING);
+  $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_STRING);
+  $pass = filter_var(trim($_POST['pass']), FILTER_SANITIZE_STRING);
+
+  if(mb_strlen($login) < 5 || mb_strlen($login) > 90) {
+    echo "Недопутимая длинна логина!";
+    exit();
+  }
 ?>
