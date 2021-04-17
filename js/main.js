36cc7da1545a91e9c76a63f01824c4e677d5bd73
Malinnikov94 <malinnikov94@gmail.com>
2021-04-17 22:16:10 +0200

    1

--- js/main.js
+++ js/main.js
@@ -17,8 +17,9 @@
 let text = 'String';                                  // Строка - string - str
 let numbers = 10;                                     // Число - integer - int
 let name = ['Nickolay', 'Ivan', 'Roman', 'Bogdan'];   // Массив - array
-  console.log(name[1]);                               // Вывод переменной
+  console.log(name[1]);                               // Выведет из массива "Ivan", 0 = Nickolay
 
-alert (mainWindow);
 
-let mainWindow = 'Push the button!';
+
+let mainWindow3 = 'Push the button!';
+alert (mainWindow3);
